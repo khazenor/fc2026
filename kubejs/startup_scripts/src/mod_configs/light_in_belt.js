a5953d163eb1f8adad78a31e473ac18c564aa2af
khazenor <80185732+khazenor@users.noreply.github.com>
2026-01-06 09:01:50 -0600

    add and support dynamic lights

---
+++ kubejs/startup_scripts/src/mod_configs/light_in_belt.js
@@ -0,0 +1,11 @@
+RequestHandler.tags.item.add([['curios:belt', [
+  "minecraft:torch",
+  "minecraft:lantern",
+  "minecraft:soul_lantern",
+  'minecraft:sea_lantern',
+  'minecraft:shroomlight',
+  'minecraft:ochre_froglight',
+  'minecraft:pearlescent_froglight',
+  'minecraft:verdant_froglight',
+  'create:experience_block'
+]]])
