8fceb79a3cb1f7dd3ef62ddeb930a7133efb85b9
khazenor <80185732+khazenor@users.noreply.github.com>
2026-01-08 14:57:06 -0600

    Revert "validate that the food collected is indeed still valid before displaying"
    This reverts commit b910b312087e4d4d78fd534adb171c412adac7f5.

--- kubejs/startup_scripts/src/core_mechanics/selling_food/player_food_collected_logger.js
+++ kubejs/startup_scripts/src/core_mechanics/selling_food/player_food_collected_logger.js
@@ -4,16 +4,9 @@ const PlayerFoodCollectedLogger = {
     PlayerCollectionLogger.logCollectible(event, foodId, this.playerDataKey)
   },
   foodCollected (event) {
-    let storedItems = PlayerCollectionLogger.collectiblesInCollection(
+    return PlayerCollectionLogger.collectiblesInCollection(
       event, SellingFoodList.foods, this.playerDataKey
     )
-    let foodCollected = []
-    for (let item of storedItems) {
-      if (SellingFoodList.foods.includes(item)) {
-        foodCollected.push(item)
-      }
-    }
-    return foodCollected
   },
   clearFoods (event) {
     PlayerCollectionLogger.clearCollection(event, this.playerDataKey)
