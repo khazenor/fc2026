ca8ede9870dbc875ab1b95fc766103d77697c37f
khazenor <80185732+khazenor@users.noreply.github.com>
2026-01-09 23:07:20 -0600

    pamela trades

--- kubejs/startup_scripts/src/core_mechanics/npcs/pamela.js
+++ kubejs/startup_scripts/src/core_mechanics/npcs/pamela.js
@@ -2,7 +2,19 @@
 const NpcPamela = {
   name: 'Pamela',
   offerDefs: [
-    {villagerItems: ['minecraft:stick']}
+    {villagerItems: [
+      'minecraft:honey_bottle',
+      'minecraft:sugar',
+      'meadow:alpine_salt',
+      'meadow:rennet',
+      'farm_and_charm:butter',
+      'farm_and_charm:dough',
+      'bakery:cake_dough',
+      'bakery:sweet_dough',
+      'create_confectionery:gingerdough',
+      'create_confectionery:cocoa_butter',
+      'create:dough'
+    ], villagerNum: 8}
   ]
 }
 
