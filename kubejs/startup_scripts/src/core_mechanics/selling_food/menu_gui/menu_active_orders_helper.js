ad14fcdbdd10613924b13fe83de8ac88c44a9fc7
khazenor <80185732+khazenor@users.noreply.github.com>
2026-01-16 10:13:54 -0600

    fix menu gui second row

--- kubejs/startup_scripts/src/core_mechanics/selling_food/menu_gui/menu_active_orders_helper.js
+++ kubejs/startup_scripts/src/core_mechanics/selling_food/menu_gui/menu_active_orders_helper.js
@@ -3,24 +3,28 @@ const MenuActiveOrdersHelper = {
     let orderStartRow = 1
     let activeOrders = PlayerOrderLogger.allOrders(event)
     ChestGuiHelper.writeArrToMatrix(
-      matrix, orderStartRow, 0, 0, 1, this.customerHeadMatrixSlots(activeOrders).slice(0, 9)
+      matrix, orderStartRow, 0, 0, 1, this.customerHeadMatrixSlots(activeOrders, 1)
     )
     ChestGuiHelper.writeArrToMatrix(
-      matrix, orderStartRow + 1, 0, 0, 1, this.foodRequestsMatrixSlots(activeOrders).slice(0, 9)
+      matrix, orderStartRow + 1, 0, 0, 1, this.foodRequestsMatrixSlots(activeOrders, 1)
     )
     ChestGuiHelper.writeArrToMatrix(
-      matrix, orderStartRow + 2, 0, 0, 1, this.customerHeadMatrixSlots(activeOrders).slice(9, 9)
+      matrix, orderStartRow + 2, 0, 0, 1, this.customerHeadMatrixSlots(activeOrders, 2)
     )
     ChestGuiHelper.writeArrToMatrix(
-      matrix, orderStartRow + 3, 0, 0, 1, this.foodRequestsMatrixSlots(activeOrders).slice(9, 9)
+      matrix, orderStartRow + 3, 0, 0, 1, this.foodRequestsMatrixSlots(activeOrders, 2)
     )
   },
-  customerHeadMatrixSlots (activeOrders) {
-    let customerNames = Object.keys(activeOrders)
+  customerHeadMatrixSlots (activeOrders, rowNum) {
+    let customerNames = rowNum === 1
+      ? Object.keys(activeOrders).slice(0, 9)
+      : Object.keys(activeOrders).slice(9, 18)
     return customerNames.map(name => this.customerHeadId(name))
   },
-  foodRequestsMatrixSlots (activeOrders) {
-    return Object.values(activeOrders)
+  foodRequestsMatrixSlots (activeOrders, rowNum) {
+    return rowNum === 1
+      ? Object.values(activeOrders).slice(0, 9)
+      : Object.values(activeOrders).slice(9, 18)
   },
   customerHeadId (customerName) {
     return `kubejs:${customerName.toLowerCase()}`
