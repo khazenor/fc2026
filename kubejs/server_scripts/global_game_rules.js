b23f8693eac1b6d2596dea7b5943d29470fae02a
khazenor <80185732+khazenor@users.noreply.github.com>
2026-01-20 16:03:31 -0600

    keep inventory rule

--- kubejs/server_scripts/global_game_rules.js
+++ kubejs/server_scripts/global_game_rules.js
@@ -4,4 +4,5 @@ ServerEvents.loaded(e => {
   e.server.gameRules.set("disableRaids", true)
   e.server.gameRules.set("playersSleepingPercentage", 0)
   e.server.gameRules.set("doTraderSpawning", false)
+  e.server.gameRules.set("keepInventory", true)
 })
