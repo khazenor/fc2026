2c3187aebd9b77ece9126a13680cb58313d47c63
khazenor <80185732+khazenor@users.noreply.github.com>
2026-01-23 22:24:02 -0600

    modify grapevine recipe to not conflict with logs to stick QOL recipe

--- kubejs/startup_scripts/src/mod_configs/lets_do_support.js
+++ kubejs/startup_scripts/src/mod_configs/lets_do_support.js
@@ -4,8 +4,13 @@ RequestHandler.recipes.add.shapeless([['vinery:dark_cherry_sapling', [
   '#minecraft:saplings', '3x minecraft:apple'
 ]]])
 
+RequestHandler.recipes.add.shaped([
+  ['vinery:grapevine_stem', [' L ', ' L ', ' L '], {L: "#minecraft:logs"}]
+])
+
 RequestHandler.recipes.remove.byItemId([
-  'farm_and_charm:chicken_coop' // disable until it doesn't crash the game
+  'farm_and_charm:chicken_coop', // disable until it doesn't crash the game
+  'vinery:grapevine_stem'
 ])
 
 RequestHandler.tooltips.add([
