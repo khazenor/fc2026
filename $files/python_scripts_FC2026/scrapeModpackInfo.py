da434526db9b500eda5774a4f38ada6506a35635
khazenor <80185732+khazenor@users.noreply.github.com>
2026-02-06 16:30:49 -0600

    rename scraped files

--- $files/python_scripts_FC2026/scrapeModpackInfo.py
+++ $files/python_scripts_FC2026/scrapeModpackInfo.py
@@ -3,17 +3,17 @@ import os
 registryFolder = '..\\..\\local\\kubejs\\export\\registries'
 outFolder = 'modpack_info'
 filenames= [
-	'item.json',
-	'block.json',
-	'entity_type.json',
-	'enchantment.json'
+	'item',
+	'block',
+	'entity_type',
+	'enchantment'
 ]
 
 if __name__ == '__main__':
 	for filename in filenames:
-		info = json.load(open(os.path.join(registryFolder, filename), 'r'))
+		info = json.load(open(os.path.join(registryFolder, filename+".json"), 'r'))
 		sortedKeys = list(info.keys())
 		sortedKeys.sort()
-		with open(os.path.join(outFolder, filename), 'w') as f:
+		with open(os.path.join(outFolder, filename+'.txt'), 'w') as f:
 			for sortedKey in sortedKeys:
 				f.write(sortedKey+'\n')
