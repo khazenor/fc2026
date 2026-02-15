e6a2c9ec3341af1f3a9d1ad10885bd2680c797ae
khazenor <80185732+khazenor@users.noreply.github.com>
2026-02-14 22:17:37 -0600

    reset size instead of weight for fish

--- kubejs/startup_scripts/src/mod_configs/tide_support/reset_size.js
+++ kubejs/startup_scripts/src/mod_configs/tide_support/reset_size.js
@@ -3,17 +3,17 @@ RequestHandler.callbacks.itemEvents.rightClicked([event => {
   let isFish = Object.keys(FishInfo).includes(itemId)
   let isShifting = EventHelpers.isPlayerShifting(event)
   if (isFish && isShifting) {
-    if (PlayerTimingJs.checkAreYouSureLike(event.player, 'resetFishWeight', 10)) {
+    if (PlayerTimingJs.checkAreYouSureLike(event.player, 'resetFishSize', 10)) {
       event.player.mainHandItem.count --
       GiveItem.giveItemsSmart(event, itemId, 1)
-      event.player.tell(Text.translate('tideSupport.resetWeight.confirmation'))
+      event.player.tell(Text.translate('tideSupport.resetSize.confirmation'))
     } else {
-      event.player.tell(Text.translate('tideSupport.resetWeight.areYouSure'))
+      event.player.tell(Text.translate('tideSupport.resetSize.areYouSure'))
     }
   }
 }])
 
 RequestHandler.tooltips.addSingular(Object.keys(FishInfo), [
-  Text.translate('tideSupport.resetWeight.tooltip1'),
-  Text.translate('tideSupport.resetWeight.tooltip2')
+  Text.translate('tideSupport.resetSize.tooltip1'),
+  Text.translate('tideSupport.resetSize.tooltip2')
 ])
