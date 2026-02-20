a977c27699f13b12b95e8b5cc2dd0dcc0014cac3
khazenor <80185732+khazenor@users.noreply.github.com>
2026-02-20 08:16:50 -0600

    Zombie sieges should now be disabled

--- $files/python_scripts_FC2026/scrapeModpackInfo.py
+++ $files/python_scripts_FC2026/scrapeModpackInfo.py
@@ -1,19 +1,24 @@
 import json
 import os
-registryFolder = '..\\..\\local\\kubejs\\export\\registries'
 outFolder = 'modpack_info'
-filenames= [
-	'item',
-	'block',
-	'entity_type',
-	'enchantment'
-]
+foldersAndFilenames = {
+	'..\\..\\local\\kubejs\\export\\registries': [
+		'item',
+		'block',
+		'entity_type',
+		'enchantment'
+	],
+	'..\\..\\local\\kubejs\\export\\registries\\worldgen': [
+		'biome'
+	]
+}
 
 if __name__ == '__main__':
-	for filename in filenames:
-		info = json.load(open(os.path.join(registryFolder, filename+".json"), 'r'))
-		sortedKeys = list(info.keys())
-		sortedKeys.sort()
-		with open(os.path.join(outFolder, filename+'.txt'), 'w') as f:
-			for sortedKey in sortedKeys:
-				f.write(sortedKey+'\n')
+	for folder in foldersAndFilenames.keys():
+		for filename in foldersAndFilenames[folder]:
+			info = json.load(open(os.path.join(folder, filename+".json"), 'r'))
+			sortedKeys = list(info.keys())
+			sortedKeys.sort()
+			with open(os.path.join(outFolder, filename+'.txt'), 'w') as f:
+				for sortedKey in sortedKeys:
+					f.write(sortedKey+'\n')
