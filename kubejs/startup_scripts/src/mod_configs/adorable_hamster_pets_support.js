df7202919ac813d0875b0d4d5989b43b904c3298
khazenor <80185732+khazenor@users.noreply.github.com>
2026-02-25 21:28:42 -0600

    cheese from adorable hamster pets can now be crafted normally

--- kubejs/startup_scripts/src/mod_configs/adorable_hamster_pets_support.js
+++ kubejs/startup_scripts/src/mod_configs/adorable_hamster_pets_support.js
@@ -1,21 +1,3 @@
-RequestHandler.recipes.remove.byItemId([
+RequestHandler.tags.item.removeAll([
   'adorablehamsterpets:cheese'
 ])
-
-RequestHandler.recipes.add.shapeless([
-  'meadow:amethyst_cheese_slice',
-  'meadow:buffalo_cheese_slice',
-  'meadow:cheese_slice',
-  'meadow:cheese_tart_slice',
-  'meadow:cheesecake_slice',
-  'meadow:goat_cheese_slice',
-  'meadow:grain_cheese_slice',
-  'meadow:sheep_cheese_slice',
-  'meadow:warped_cheese_slice'
-].map(slice => [
-  'adorablehamsterpets:cheese', [slice]
-]))
-
-RequestHandler.recipes.add.shapeless([[
-  'meadow:cheese_slice', ['adorablehamsterpets:cheese']
-]])
